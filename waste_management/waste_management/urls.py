36514ce1cc8a7c0d89a1ed8e3f09a95b1c18e005
Aarjav Godha <aarjavgodha@Aarjavs-MacBook-Pro.local>
2026-04-16 23:26:03 +0530

    fix: Unignore seed images and serve media in production URL rules

--- waste_management/waste_management/urls.py
+++ waste_management/waste_management/urls.py
@@ -39,6 +39,12 @@ urlpatterns = [
     path('logout', LogoutView.as_view(next_page='/login'), name='logout'),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
